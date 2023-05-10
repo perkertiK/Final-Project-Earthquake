388c96e0d1b9d3fac4ba2b869cdc1f1175378029
Perkerti Koirala <78633309+Perkerti69@users.noreply.github.com>
2023-05-10 13:53:03 -0400

    chnages to project

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-
-//map initialization
+// Initialize the map
 const map = L.map("mapid").setView([0, 0], 2);
-//tile layer of map
+
+// Add the tile layer (the base map)
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: 
-        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-     maxZoom: 18
+  attribution:
+    'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18
 }).addTo(map);
 
+// Set up the marker clusters layer
 let markers = L.markerClusterGroup({
   spiderfyOnMaxZoom: true,
   showCoverageOnHover: false,
   zoomToBoundsOnClick: true
 });
-
 
 // Fetch earthquake data from USGS API
 async function getEarthquakeData() {
@@ -51,7 +51,72 @@ getEarthquakeData().then(quakes => {
 
   // Add the marker clusters layer to the map
   map.addLayer(markers);
+
+  // Add a text box filter to the earthquake data
+  const filterInput = document.createElement('input');
+  filterInput.setAttribute('type', 'text');
+  filterInput.setAttribute('placeholder', 'Filter earthquakes by location');
+  filterInput.addEventListener('input', (event) => {
+    const query = event.target.value.toLowerCase();
+    markers.clearLayers();
+    quakeMarkers.forEach(quake => {
+      if (quake.title.toLowerCase().includes(query)) {
+        const marker = L.marker(quake.latlng, {
+          title: quake.title,
+          icon: L.divIcon({
+            className: "marker",
+            html: "<span>" + quake.magnitude + "</span>"
+          })
+        });
+        marker.bindPopup("<h3>" + quake.title + "</h3><p><strong>Magnitude:</strong> " + quake.magnitude + "</p><p><strong>Depth:</strong> " + quake.depth + " km</p>");
+        markers.addLayer(marker);
+      }
+    });
+  });
+
+  // Add the filter input to the page
+  const searchBox = document.querySelector('.search-box');
+  searchBox.appendChild(filterInput);
 });
+const searchButton = document.querySelector('#search-btn');
+
+searchButton.addEventListener('click', () => {
+  const searchText = document.querySelector('#search-text').value;
+
+  // Perform the API request using fetch()
+  fetch(`https://api.example.com/search?q=${searchText}`)
+    .then(response => response.json())
+    .then(data => {
+      // Process the response data and display it on the page
+      const resultsContainer = document.querySelector('#search-results');
+      resultsContainer.innerHTML = '';
+
+      if (data.results.length === 0) {
+        resultsContainer.innerHTML = 'No results found.';
+      } else {
+        data.results.forEach(result => {
+          const resultDiv = document.createElement('div');
+          resultDiv.classList.add('result');
+
+          const title = document.createElement('h2');
+          title.innerText = result.title;
+
+          const description = document.createElement('p');
+          description.innerText = result.description;
+
+          resultDiv.appendChild(title);
+          resultDiv.appendChild(description);
+          resultsContainer.appendChild(resultDiv);
+        });
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      // Display an error message on the page
+      const resultsContainer = document.querySelector('#search-results');
+      resultsContainer.innerHTML = 'An error occurred. Please try again later.';
+    });
 
 
-document.addEventListener('DOMContentLoaded', async () => getEarthquakeData()); // the async keyword means we can make API requests
+
+});
